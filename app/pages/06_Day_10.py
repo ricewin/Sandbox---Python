77c039976596c6b5f205184bf5334430bc11732b
ricewin <101691318+ricewin@users.noreply.github.com>
2024-03-14 11:46:00 +0000

    Append

---
+++ app/pages/06_Day_10.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("30 Days of Streamlit")
+st.markdown("### Day 10")
+
+st.header("st.selectbox")
+
+option = st.selectbox("What is your favorite color?", ("Blue", "Red", "Green"))
+
+st.write("Your favorite color is ", option)
