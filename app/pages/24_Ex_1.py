9086a7b41ceaa055817349e8a3dc0702de1df137
Ricewin <101691318+ricewin@users.noreply.github.com>
2024-05-21 11:34:49 +0000

    🍉

--- app/pages/24_Ex_1.py
+++ app/pages/24_Ex_1.py
@@ -4,27 +4,37 @@ import streamlit as st
 from dateutil.relativedelta import relativedelta
 
 st.title("確認用")
-st.header("日付リスト")
+st.header("日付リスト生成", divider="rainbow")
 
-# 日付を作成する期間をセット
-span = st.radio(
-    "取得する期間を選択",
-    [6, 12, 18, 24, 48, 60, 72],
-    horizontal=True,
-)
+col1, col2, col3 = st.columns(3)
 
-# 基準月をセット
-position = st.radio(
-    "ベーステーブルを選択",
-    [1, 2, 3, 4, 5, 6, 7],
-    horizontal=True,
-)
+with col1:
+    position = st.radio(
+        "基準月を選択",
+        [0, 1, 2, 3, 4, 5, 6, 7],
+        horizontal=True,
+    )
+
+with col2:
+    date_span = st.radio(
+        "日付を作成する期間を選択",
+        [6, 12, 18, 24, 48, 60, 72],
+        horizontal=True,
+    )
+
+with col3:
+    past_future = st.radio(
+        "過去・未来",
+        ["過去", "未来"],
+        horizontal=True,
+    )
+
 
 # 基準月をセット
 start_date = datetime.now() - relativedelta(months=position)
 
 # 降順でリストにする
-dates = [start_date - relativedelta(months=i) for i in range(span)]  # type: ignore
+dates = [start_date - relativedelta(months=i) for i in range(date_span)]  # type: ignore
 
 selected_month = st.selectbox(
     "抽出したい利用年月を選んでください",
@@ -34,4 +44,13 @@ selected_month = st.selectbox(
 
 # Debug
 st.write(selected_month)
+# st.write(dates)
+
+if past_future == "過去":
+    dates = [start_date - relativedelta(months=i) for i in range(date_span)]  # type: ignore
+
+if past_future == "未来":
+    dates = [start_date + relativedelta(months=i + 1) for i in range(date_span)]  # type: ignore
+
+# Debug
 st.write(dates)
