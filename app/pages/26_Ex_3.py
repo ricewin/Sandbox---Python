0c55f3c03880576125e01de8b4a186e9af8c778b
ricewin <hiroccha@gmail.com>
2024-06-15 16:42:39 +0900

    add

---
+++ app/pages/26_Ex_3.py
@@ -0,0 +1,79 @@
+from io import StringIO
+
+import folium
+import pandas as pd
+import streamlit as st
+from folium.plugins import HeatMap, MarkerCluster
+from streamlit_folium import st_folium
+
+st.set_page_config(layout="wide")
+
+st.title(":satellite: マッピング")
+st.subheader(
+    ":mount_fuji: 緯度・経度が入った CSV ファイルからマップ表示するテスト",
+    divider="rainbow",
+)
+
+upload_file = st.file_uploader("Choose a file")
+if upload_file is not None:
+    bytes_data = upload_file.getvalue()
+    stringio = StringIO(upload_file.getvalue().decode("utf-8"))
+    string_data = stringio.read()
+    data = pd.read_csv(upload_file)
+
+    if "latitude" in data.columns and "longitude" in data.columns:
+        st.info("実行ボタンをクリックするとマップを表示します。")
+    else:
+        with st.expander("データテーブルの編集", expanded=True):
+            st.info(
+                "緯度・経度のあるカラム名を「緯度」「経度」に書き換えて実行すると、マップ表示ができます。"
+            )
+
+            st.write("データの確認:", data)
+
+            # カラム名の編集フィールドを作成する
+            new_columns = []
+            for col in data.columns:
+                new_col = st.text_input(f"{col} の新しい名前", value=col)
+                new_columns.append(new_col)
+
+            # 新しいカラム名を適用する
+            data.columns = new_columns
+
+    if st.button(":mag_right: 実行"):
+        try:
+            # 位置情報のないレコードをドロップ
+            df = data.dropna(subset=["緯度", "経度"])
+
+            # 地図の初期位置を設定
+            map_center = [df["緯度"].mean(), df["経度"].mean()]
+
+        except KeyError:
+            st.error(
+                "地図表示できません。「緯度」「経度」を指定しているか確認してください。"
+            )
+            st.stop()
+
+        except TypeError:
+            st.error("地図表示できません。間違った列を指定しています。")
+            st.stop()
+
+        st.write(f"有効件数: {len(df)} / {len(data)}")
+
+        # マップを作成
+        map = folium.Map(location=map_center, zoom_start=8)
+
+        # ピンをグループ化するマーカーグループクラスタを作成
+        marker_cluster = MarkerCluster().add_to(map)
+
+        # データフレームの各行に対して、ピンを作成してマップに追加
+        for index, row in df.iterrows():
+            folium.Marker(
+                location=[row["緯度"], row["経度"]],
+            ).add_to(marker_cluster)
+
+        # ヒートマップ表示を追加
+        coordinates = df[["緯度", "経度"]].values.tolist()
+        HeatMap(coordinates).add_to(marker_cluster)
+
+        st_folium(map, use_container_width=True, returned_objects=[])
