fcbea39d7d77e6f6fc2305359900d3f1676f58ab
Ricewin <101691318+ricewin@users.noreply.github.com>
2024-06-16 07:28:01 +0000

    🧮

--- app/pages/26_Ex_3.py
+++ app/pages/26_Ex_3.py
@@ -1,9 +1,11 @@
 from io import StringIO
+from typing import Any
 
 import folium
 import pandas as pd
 import streamlit as st
 from folium.plugins import HeatMap, MarkerCluster
+from streamlit.runtime.uploaded_file_manager import UploadedFile
 from streamlit_folium import st_folium
 
 st.set_page_config(layout="wide")
@@ -14,27 +16,28 @@ st.subheader(
     divider="rainbow",
 )
 
-upload_file = st.file_uploader("Choose a file")
-if upload_file is not None:
-    bytes_data = upload_file.getvalue()
-    stringio = StringIO(upload_file.getvalue().decode("utf-8"))
-    string_data = stringio.read()
-    data = pd.read_csv(upload_file)
+upload_file: UploadedFile | None = st.file_uploader("Choose a file")
 
-    if "latitude" in data.columns and "longitude" in data.columns:
+if upload_file is not None:
+    bytes_data: bytes = upload_file.getvalue()
+    stringio = StringIO(upload_file.getvalue().decode("utf-8"))
+    string_data: str = stringio.read()
+    data: pd.DataFrame = pd.read_csv(upload_file)
+
+    if "緯度" in data.columns and "経度" in data.columns:
         st.info("実行ボタンをクリックするとマップを表示します。")
     else:
-        with st.expander("データテーブルの編集", expanded=True):
+        with st.expander("データフレームの編集", expanded=True):
             st.info(
                 "緯度・経度のあるカラム名を「緯度」「経度」に書き換えて実行すると、マップ表示ができます。"
             )
 
-            st.write("データの確認:", data)
+            st.write(data)
 
             # カラム名の編集フィールドを作成する
-            new_columns = []
+            new_columns: list[Any] = []
             for col in data.columns:
-                new_col = st.text_input(f"{col} の新しい名前", value=col)
+                new_col: str = st.text_input(f"{col} の新しい名前", value=col)
                 new_columns.append(new_col)
 
             # 新しいカラム名を適用する
@@ -43,10 +46,10 @@ if upload_file is not None:
     if st.button(":mag_right: 実行"):
         try:
             # 位置情報のないレコードをドロップ
-            df = data.dropna(subset=["緯度", "経度"])
+            df: pd.DataFrame = data.dropna(subset=["緯度", "経度"])
 
             # 地図の初期位置を設定
-            map_center = [df["緯度"].mean(), df["経度"].mean()]
+            map_center: list[float] = [df["緯度"].mean(), df["経度"].mean()]
 
         except KeyError:
             st.error(
@@ -64,16 +67,17 @@ if upload_file is not None:
         map = folium.Map(location=map_center, zoom_start=8)
 
         # ピンをグループ化するマーカーグループクラスタを作成
-        marker_cluster = MarkerCluster().add_to(map)
+        marker_cluster: MarkerCluster = MarkerCluster().add_to(map)
 
         # データフレームの各行に対して、ピンを作成してマップに追加
         for index, row in df.iterrows():
             folium.Marker(
                 location=[row["緯度"], row["経度"]],
+                icon=folium.Icon(color="blue"),
             ).add_to(marker_cluster)
 
         # ヒートマップ表示を追加
-        coordinates = df[["緯度", "経度"]].values.tolist()
+        coordinates: Any = df[["緯度", "経度"]].values.tolist()
         HeatMap(coordinates).add_to(marker_cluster)
 
         st_folium(map, use_container_width=True, returned_objects=[])
