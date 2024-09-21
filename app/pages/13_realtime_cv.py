7ee6e3c58801170c01874209526892c87113bc42
ricewin <hiroccha@gmail.com>
2024-09-21 22:58:41 +0900

    🍩

--- app/pages/13_realtime_cv.py
+++ app/pages/13_realtime_cv.py
@@ -1,7 +1,7 @@
-import av
-import cv2
+import av  # type: ignore
+import cv2  # type: ignore
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer
+from streamlit_webrtc import webrtc_streamer  # type: ignore
 
 # ブラウザで動くリアルタイム画像/音声処理アプリをStreamlitでサクッと作る
 # https://zenn.dev/whitphx/articles/streamlit-realtime-cv-app
@@ -16,7 +16,7 @@ threshold2 = st.slider("Threshold2", min_value=0, max_value=1000, step=1, value=
 def callback(frame):
     img = frame.to_ndarray(format="bgr24")
 
-    # img = cv2.cvtColor(cv2.Canny(img, threshold1, threshold2), cv2.COLOR_GRAY2BGR)
+    img = cv2.cvtColor(cv2.Canny(img, threshold1, threshold2), cv2.COLOR_GRAY2BGR)
 
     return av.VideoFrame.from_ndarray(img, format="bgr24")
 
